d6aa817f366fa251af401dde1b2a149b00659ff9
Romane BOULIER <pro.boulier.romane@gmail.com>
2025-04-17 16:08:19 +0200

    maj majeure du script (inscription, connexion, profil)

--- script.js
+++ script.js
@@ -3,40 +3,55 @@
 window.onload = init;
 
 function init() {
-    document.getElementById("username").addEventListener("input", function () {
-        checkUsername();
-        submit();
-    });
-    document.getElementById("userMail").addEventListener("input", function () {
-        checkUserMail();
-        submit();
-    });
-    document.getElementById("password").addEventListener("input", function () {
-        pwdStrength();
-        comparePwd();
-        submit();
-    });
-    document.getElementById("passwordBis").addEventListener("input", function () {
-        comparePwd();
-        submit();
-    });
 
-    // Appel initial des fonctions pour configurer l'état initial
-    checkUsername();
-    checkUserMail();
-    comparePwd();
-    pwdStrength();
-    submit();
-    // connexion();
+    // Page d'inscription
 
-    document.getElementById("formulaireInscription").addEventListener("submit", function (event) {
-        event.preventDefault();
-        submit();
-        if (!document.getElementById("validerInscription").disabled) {
-            stockageUser();
-            window.location.href = 'connexion.html';
+    if (document.getElementById("formulaireInscription")) {
+        document.getElementById("username").addEventListener("input", function () {
+            checkUsername();
+            submit();
+        });
+        document.getElementById("userMail").addEventListener("input", function () {
+            checkUserMail();
+            submit();
+        });
+        document.getElementById("password").addEventListener("input", function () {
+            pwdStrength();
+            comparePwd();
+            submit();
+        });
+
+        document.getElementById("passwordBis").addEventListener("input", function () {
+            comparePwd();
+            submit();
+        });
+
+        document.getElementById("formulaireInscription").addEventListener("submit", function (event) {
+            event.preventDefault();
+            submit();
+            if (!document.getElementById("validerInscription").disabled) {
+                stockageUser();
+                window.location.href = 'connexion.html';
+            }
+        });
+    }
+
+    // Page de connexion
+
+    if (document.getElementById("formulaireConnexion")) {
+        document.getElementById("formulaireConnexion").addEventListener("submit", connexion);
+        document.getElementById("formulaireConnexion").addEventListener("submit", stockageUserConnexion);
+    }
+
+    // Page de profil
+    if (document.getElementById("formulaireProfil")) {
+        let userCo = sessionStorage.getItem("userCo")
+        let afficherUser = document.getElementById("userCo")
+
+        if (userCo !== null) {
+            afficherUser.textContent = userCo;
         }
-    });
+    }
 }
 
 function checkUsername() {
@@ -156,13 +171,18 @@ function comparePwd() {
         document.getElementById("passwordBisError").style.display = "none"
         // validation.textContent = "✓ Les mots de passe correspondent";
         // validation.style.color = "green";
-    } 
+    }
     if (pwd1 !== pwd2) {
         document.getElementById("passwordBisError").style.display = "block"
         // validation.textContent = "✗ Les mots de passe ne correspondent pas";
         // validation.style.color = "red";
-    } 
+    }
 }
+
+/* Fonction de soumission du formulaire :
+- vérifie que l'username et le mail n'existe pas dans le local storage.
+- vérifie que tous les champs correspondent aux attendus. 
+- si tout est ok, le bouton est cliquable. */
 
 function submit() {
     // Vérification dans le localStorage
@@ -201,29 +221,100 @@ function submit() {
     }
 }
 
+/* Fonction de stockage :
+- récupère les valeurs de l'username, usermail, password.
+- stocke tout ça dans une variable, sous forme d'objet JS.
+- on récupère les données du storage, s'il n'y a rien, on crée un tableau.
+- on ajoute les nouvelles données à la liste des utilisateurs.
+- on envoie ces données dans le storage.
+ */
+
 function stockageUser() {
     const username = document.getElementById("username").value;
     const userMail = document.getElementById("userMail").value;
     const password = document.getElementById("password").value;
 
-    // Créer un objet pour le nouvel utilisateur
     const newUser = {
         username: username,
         userMail: userMail,
         password: password
     };
 
-    // Récupérer la liste des utilisateurs existants ou créer un tableau vide
     let usersList = JSON.parse(localStorage.getItem('usersList')) || [];
-    
-    // Ajouter le nouvel utilisateur à la liste
+
     usersList.push(newUser);
-    
-    // Sauvegarder la liste mise à jour dans localStorage
+
     localStorage.setItem('usersList', JSON.stringify(usersList));
-    
-    // Optionnel : Vider les champs du formulaire
-    document.getElementById("username").value = "";
-    document.getElementById("userMail").value = "";
-    document.getElementById("password").value = "";
+}
+
+/* PROBLEME : Fonction de connexion :
+- on récupère les valeurs des input du formulaire.
+- on les compare avec les données du storage.
+- si l'username et le mdp correspondent : on peut cliquer et passer sur la page profil.
+- sinon, alerte. PROBLEME ICI */
+
+function connexion(event) {
+    console.log("plop")
+    // - on récupère les valeurs des input du formulaire.
+    let usernameCo = document.getElementById("usernameCo").value
+    let passwordCo = document.getElementById("passwordCo").value
+    let usersList = JSON.parse(localStorage.getItem('usersList'))
+    let usernameCoValide = false;
+    let loginValide = false;
+    let btnConnexion = document.getElementById("btnConnexion")
+
+    // On sort de la fonction si un champ est vide
+    if (usernameCo === "" || passwordCo === "") {
+        btnConnexion.disabled = true;
+        return;
+    }
+
+    // - on les compare avec les données du storage : boucle for pour parcourir usersList.
+
+    for (let index = 0; index < usersList.length; index++) {
+        usernameCoValide = usersList[index].username === usernameCo
+        passwordCoValide = usersList[index].password === passwordCo
+        loginValide = usernameCoValide && passwordCoValide
+        if (loginValide) {
+            break
+        }
+    }
+
+    if (loginValide) {
+        btnConnexion.disabled = false;
+        event.target.action = "profil.html"
+    } else {
+        // On affiche seulement l'alerte si les deux champs sont remplis mais invalides
+        if (usernameCo !== "" && passwordCo !== "") {
+            btnConnexion.disabled = false;
+            alert("Erreur")
+        }
+    }
+}
+
+/* Fonction de stockage des données de connexion dans le session storage :
+- - récupère les valeurs de l'username, usermail, password.
+- stocke tout ça dans une variable, sous forme d'objet JS.
+- on récupère les données du storage, s'il n'y a rien, on crée un tableau.
+- on ajoute les nouvelles données à la liste des utilisateurs.
+- on envoie ces données dans le storage.
+ */
+
+function stockageUserConnexion() {
+    let usernameCo = document.getElementById("usernameCo").value
+
+    sessionStorage.setItem("userCo", usernameCo)
+}
+
+/* Fonction pour afficher en readonly l'username et l'adresse mail de la personne connectée */
+
+function afficherUserCo() {
+    console.log("hello")
+    let userCo = sessionStorage.getItem("userCo")
+    let afficherUser = document.getElementById("userCo")
+
+    if (userCo !== null) {
+        afficherUser.textContent = userCo;
+    }
+
 }
